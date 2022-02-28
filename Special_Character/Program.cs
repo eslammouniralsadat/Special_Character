45e4e75797e695427acbdc15ec61d93a2ca11fe0
Eslam Mounir AL Sadat <alsadateslammounir@hotmail.com>
2022-02-28 20:40:17 +0200

    till end

--- Special_Character/Program.cs
+++ Special_Character/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("My Name is Eslam Mounir AL Sadat");
+            // Special Characters in Strings For C#
+
+            Console.WriteLine("My Name is \' Eslam Mounir AL Sadat"); // => Single Quote => \'
+            Console.WriteLine("My Name is \" Eslam Mounir AL Sadat"); // => Double Quote => \"
+            Console.WriteLine("My Name is \\ Eslam Mounir AL Sadat"); // => Backslash => \\
+            Console.WriteLine("My Name is \r Eslam Mounir AL Sadat"); // => Carriage Return => \r
+            Console.WriteLine("My Name is \t Eslam Mounir AL Sadat"); // => Tab => \t
+            Console.WriteLine("My Name is \b \b Eslam Mounir AL Sadat"); // => Backspace => \b
+            Console.WriteLine("My Name is \f Eslam Mounir AL Sadat"); // => Form Feed => \f
+            Console.WriteLine("My Name is \n Eslam Mounir AL Sadat"); // => New Line => \n
         }
     }
 }
