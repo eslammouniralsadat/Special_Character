6fbf82b44e9ec396089f1deb6ce0c51de1803c1c
Eslam Mounir AL Sadat <alsadateslammounir@hotmail.com>
2022-02-28 20:31:05 +0200

    Add project files.

---
+++ Special_Character/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My Name is Eslam Mounir AL Sadat");
+        }
+    }
+}
